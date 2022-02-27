0e195415ec3fd54502664fc92916ca61bf005921
Promise <me@promise.solutions>
2022-02-27 19:00:23 +0100

    Fix functions

--- functions/api/stats.ts
+++ functions/api/stats.ts
@@ -1,4 +1,13 @@
-let stats = {
+export type APIStats = {
+  guilds: number;
+  ranking: number;
+  users: number;
+  count: number;
+  servers: Record<string, number>,
+  last: number;
+}
+
+let stats: APIStats = {
   guilds: 0,
   ranking: 0,
   users: 0,
@@ -16,13 +25,18 @@ export const onRequest: PagesFunction = async request => {
   if (stats.last + 300_000 < now) {
     stats.last = now;
 
-    const { guilds, ranking } = await getDblStatistics();
+    const env = request.env as unknown as Record<string, string>;
+    const { guilds, ranking } = await getDblStatistics(env.DBL_TOKEN);
+    const users = await getCountrUsers(env.COUNTR_STATS_ENDPOINT);
+    const count = await getCountrCount(env.COUNTR_COUNT_ENDPOINT);
 
     // eslint-disable-next-line require-atomic-updates
     stats = {
       ...stats,
       guilds: Math.floor(guilds / 100) * 100,
       ranking,
+      users: Math.floor(users / 100_000) * 100_000,
+      count: Math.floor(count / 1000) * 1000,
       servers: {
         slogo: await getMembers("slogo"),
         sound: await getMembers("sound"),
@@ -34,12 +48,12 @@ export const onRequest: PagesFunction = async request => {
   return new Response(JSON.stringify(stats));
 };
 
-async function getDblStatistics() {
-  if (!process.env.DBL_TOKEN) return { guilds: 0, ranking: 0 };
+async function getDblStatistics(token?: string) {
+  if (!token) return { guilds: 0, ranking: 0 };
 
   const json = await fetch("https://dblstatistics.com/api/bots/467377486141980682", {
     headers: {
-      Authorization: process.env.DBL_TOKEN,
+      Authorization: token,
     },
   }).then(res => res.json()).catch(() => ({})) as {
     server_count?: number;
@@ -50,6 +64,22 @@ async function getDblStatistics() {
     guilds: json.server_count || 0,
     ranking: json.server_count_rank || 0,
   };
+}
+
+async function getCountrUsers(endpoint?: string) {
+  if (!endpoint) return 0;
+  const json = await fetch(endpoint).then(res => res.json()).catch(() => ({})) as {
+    users?: number;
+  };
+
+  return json.users || 0;
+}
+
+async function getCountrCount(endpoint?: string) {
+  if (!endpoint) return 0;
+  const count = parseInt(await fetch(endpoint).then(res => res.text()).catch(() => "0"));
+
+  return count || 0;
 }
 
 async function getMembers(invite: string) {
